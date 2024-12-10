26f5765b3cd10b26f5f3ff78fe296234ecc92c8f
Pavel Pasechnik <ingle1337@gmail.com>
2024-12-10 17:32:37 +0500

    Add project files.

---
+++ Tyuiu.PasechnikPA1.Sprint5.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.PasechnikPA1.Sprint5.Task0.V6.Lib
+{
+    public class DataService : ISprint5Task0V6
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask0.txt";
+            double y = Math.Round((x) / (Math.Sqrt(x * x + x)), 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.PasechnikPA1.Sprint5.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PasechnikPA1.Sprint5.Task0.V6.Lib;
+using System.IO;
+namespace Tyuiu.PasechnikPA1.Sprint5.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistFile()
+        {
+            string path = @"C:\\Users\\Павел\\source\\repos\\Tyuiu.PasechnikPA1.Sprint5\\Tyuiu.PasechnikPA1.Sprint5.Task0.V6\\bin\\Debug\\net8.0\\OutputFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            Assert.AreEqual(true, fileExist);
+        }
+    }
+}
