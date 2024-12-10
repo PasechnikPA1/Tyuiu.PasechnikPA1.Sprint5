af9c9243125899ca829745e54578eec7144ace5b
Pavel Pasechnik <ingle1337@gmail.com>
2024-12-10 17:40:22 +0500

    task0.1

--- Tyuiu.PasechnikPA1.Sprint5.Task0.V6/Program.cs
+++ Tyuiu.PasechnikPA1.Sprint5.Task0.V6/Program.cs
@@ -1,30 +1,30 @@
 ﻿using Tyuiu.PasechnikPA1.Sprint5.Task0.V6.Lib;
-using static System.Net.Mime.MediaTypeNames;
+
 namespace Tyuiu.PasechnikPA1.Sprint5.Task0.V6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Title = "Спринт #5 | Выполнил: Петров Н. Е.  | АСОиУБ-23-1";
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Замятин Д. Ю. | ИИПб 23-3";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File                                                        *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
             Console.WriteLine("* Задание #0                                                              *");
             Console.WriteLine("* Вариант #6                                                              *");
-            Console.WriteLine("* Выполнил: Петров Никита Евгеньевич | АСОиУБ-23-1                        *");
+            Console.WriteLine("* Выполнил: Замятин Дмитрий Юрьевич | ИИПб 23-3                           *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
-            Console.WriteLine("* в текстовый файл OutPutFileTask0.txt и вывести на консоль.Округлить до  *");
-            Console.WriteLine("* трёх знаков после запятой.                                              *");
+            Console.WriteLine("* УСЛОВИЕ: Дано выражение вычислить его значение при x = 3, результат     *");
+            Console.WriteLine("*  сохранить в текстовый файл OutPutFileTask0.txt и вывести на консоль.   *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            DataService ds = new DataService();
+
             int x = 3;
-            Console.WriteLine($"x = {x}");
+            Console.WriteLine("x = " + x);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
@@ -34,7 +34,8 @@ namespace Tyuiu.PasechnikPA1.Sprint5.Task0.V6
 
             Console.WriteLine("Файл: " + res);
             Console.WriteLine("Создан!");
-            Console.ReadKey();
+
+            Console.ReadLine();
         }
     }
 }
