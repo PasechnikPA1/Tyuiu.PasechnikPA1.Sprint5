cb60b6a98184827db16469a4e42e7daea0704a03
Pavel Pasechnik <ingle1337@gmail.com>
2024-12-11 14:30:07 +0500

    task1

--- Tyuiu.PasechnikPA1.Sprint5.Task1.V17.Lib/DataService.cs
+++ Tyuiu.PasechnikPA1.Sprint5.Task1.V17.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.PasechnikPA1.Sprint5.Task1.V17.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
